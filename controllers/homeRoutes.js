9caf765719214dd5bf678861fe7b52858f14e6de
CASTICRI003 <ccastillo0924@gmail.com>
2023-01-30 15:24:56 -0600

    added login route to homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -68,3 +68,13 @@ router.get('/dashboard', withAuth, async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('./dashboard');
+        return;
+    }
+    res.render('login');
+});
+
+module.exports = router;
