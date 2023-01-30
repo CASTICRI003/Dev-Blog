78afa20ebc58cb69d63d88a5d6a7bdef0ceff047
CASTICRI003 <ccastillo0924@gmail.com>
2023-01-30 15:14:47 -0600

    added post route for all posts in homeROutes

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const { Post, User, Comment } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+            ],
+        });
+
+        const posts = postData.map((post) => post.get({ plain: true }));
+
+        res.render('homepage', {
+            posts,
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
