4600a6557ad8b0162ad2b2352bbf02285687acde
CASTICRI003 <ccastillo0924@gmail.com>
2023-01-30 14:49:20 -0600

    added delete route to dashboardRoutes

--- controllers/api/dashboardRoutes.js
+++ controllers/api/dashboardRoutes.js
@@ -43,3 +43,26 @@ router.get('/dashboard', withAuth, async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+
+router.delete('/post/:id', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            },
+        });
+        
+        if (!postData) {
+            res.status(404).json({ message: 'This post no longer exists.' });
+            return;
+        }
+        
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
