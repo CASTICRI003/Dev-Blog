4ab1c6187ce93554271e39ae4b8f069935473cfb
CASTICRI003 <ccastillo0924@gmail.com>
2023-01-30 14:45:10 -0600

    set up routes files, added get route to dashboard

---
+++ controllers/api/dashboardRoutes.js
@@ -0,0 +1,45 @@
+const router = require('express').Router();
+const { User, Post, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.get('/dashboard', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.findAll(
+            {
+                where: {
+                    user_id: req.session.user_id
+                },
+                attributes: [
+                    'id',
+                    'title',
+                    'content',
+                    'post_date',
+                ],
+                include: [{
+                    model: Comment,
+                    attributes: [
+                        'id',
+                        'comment_text',
+                        'post_id',
+                        'user_id',
+                    ],
+                    include: {
+                        model: User,
+                        attributes: ['name']
+                    }
+                },
+            {
+                model: User,
+                attributes: ['name']
+            },
+        ]
+            }
+        );
+        const posts = postData.map((post) => post.get({ plain: true }));
+        res.render('dashboard', {
+            posts,
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
