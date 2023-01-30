3b4b02f426ce9b8a3f4776aaa0ffc2e5bcf2fc84
CASTICRI003 <ccastillo0924@gmail.com>
2023-01-30 16:29:00 -0600

    added functionality to adding and deleting posts

---
+++ public/js/newPost.js
@@ -0,0 +1,40 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+
+    const newPost = document.querySelector('#new-post');
+    const title = document.querySelector('#title').value;
+    const content = document.querySelector('#content').value;
+
+    if (title && content) {
+        const response = await fetch('/api/posts', {
+            method: 'POST',
+            body: JSON.stringify({ title, content}),
+            headers: {'Content-Type': 'application/json',},
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('Failed to create post.');
+        }
+    }
+};
+
+
+const deleteButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+        const id = event.target.getAttribute('data-id');
+
+        const response = await fetch(`/api/post/${id}`, {
+            method: 'DELETE',
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('Failed to delete post.');
+        }
+    }
+};
+
+document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
