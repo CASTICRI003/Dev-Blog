ec44c4ae855103eb95c57c3030c90da576806172
CASTICRI003 <ccastillo0924@gmail.com>
2023-01-30 15:07:52 -0600

    added validatition for logging in in userRoutes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -19,3 +19,33 @@ router.post('/', async (req, res) => {
     }
 });
 
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await User.findOne({
+            where: {
+                email: req.body.email
+            },
+        });
+
+        if (!userData) {
+            res.status(400).json({ message: 'Incorrect email or password, please try again.' });
+            return;
+        }
+
+        const validPassword = await userData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect email or password, please try again.' });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+        });
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
